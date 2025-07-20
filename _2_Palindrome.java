11716e7a0c462a79926d78e61b7bae9a97652064
MangesshJ <109621113+MangesshJ@users.noreply.github.com>
2025-07-20 22:01:37 +0530

    _2_Palindrome.java

--- _2_Palindrome.java
+++ _2_Palindrome.java
@@ -11,5 +11,17 @@ public class _2_Palindrome {
 			finalWord = ch + finalWord;
 		}
 		System.out.println(finalWord);
+
+// 2. Using StringBuilder : (Filter Palindrome Words) -----------------------------
+		List<String> words = Arrays.asList("Radar", "Hello", "Deified", "Madam", "Java");
+		List<String> isPalindrome = new ArrayList<>();
+		
+		for(String word : words) {
+			String reversed = new StringBuilder(word.toLowerCase()).reverse().toString();
+			if(word.toLowerCase().equals(reversed)) {
+				isPalindrome.add(word);
+			}
+		}
+		System.out.println(isPalindrome);
 	}
 }
